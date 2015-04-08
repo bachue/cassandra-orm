d6dbdfb02d460b1b59a6cd766154a7016bbbafb8
Bachue Zhou <bachue.shu@gmail.com>
2015-04-09 00:23:51 +0800

    add more test cases

--- spec/examples/model/persist_spec.rb
+++ spec/examples/model/persist_spec.rb
@@ -9,45 +9,67 @@ describe CassandraORM::Model::Persist do
     end
   end
 
-  it 'should be able to create a product' do
-    product = Product.find(name: 'cassandra')
-    expect(product).to be_nil
-    product = Product.new name: 'cassandra'
-    expect(product.save).to be true
-    expect(product).not_to be_new
-    expect(product.errors).to be_empty
-    product = Product.find(name: 'cassandra')
-    expect(product).not_to be_nil
-    expect(product).not_to be_new
-    expect(product.name).to eq 'cassandra'
+  context 'create' do
+    it 'should be able to create a product' do
+      product = Product.find(name: 'cassandra')
+      expect(product).to be_nil
+      product = Product.new name: 'cassandra'
+      expect(product.save).to be true
+      expect(product).not_to be_new
+      expect(product.errors).to be_empty
+      product = Product.find(name: 'cassandra')
+      expect(product).not_to be_nil
+      expect(product).not_to be_new
+      expect(product.name).to eq 'cassandra'
+    end
+
+    it 'should be able to create an upgrade' do
+      upgrade = Upgrade.find product_name: 'cassandra', version: 1
+      expect(upgrade).to be_nil
+      upgrade = Upgrade.new product_name: 'cassandra', version: 1,
+                            minimal_version: 0, url: 'http://cassandra.apache.org/'
+      expect(upgrade.save).to be true
+      expect(upgrade.product_name).to eq 'cassandra'
+      expect(upgrade.version).to eq 1
+      expect(upgrade.minimal_version).to eq 0
+      expect(upgrade.url).to eq 'http://cassandra.apache.org/'
+      expect(upgrade.changelog).to be_nil
+    end
+
+    it 'should be able to detect uniqueness conflict' do
+      product = Product.new name: 'cassandra'
+      expect(product.save).to be true
+      product = Product.new name: 'cassandra'
+      expect(product.save).to be false
+      expect(product.errors).to match name: :unique
+
+      upgrade = Upgrade.new product_name: 'cassandra', version: 1,
+                            minimal_version: 0, url: 'http://cassandra.apache.org/'
+      expect(upgrade.save).to be true
+      upgrade = Upgrade.new product_name: 'cassandra', version: 1,
+                            minimal_version: 0, url: 'http://cassandra.apache.org/'
+      expect(upgrade.save).to be false
+      expect(upgrade.errors).to match [:product_name, :version] => :unique
+    end
   end
 
-  it 'should be able to create an upgrade' do
-    upgrade = Upgrade.find product_name: 'cassandra', version: 1
-    expect(upgrade).to be_nil
-    upgrade = Upgrade.new product_name: 'cassandra', version: 1,
-                          minimal_version: 0, url: 'http://cassandra.apache.org/'
-    expect(upgrade.save).to be true
-    expect(upgrade.product_name).to eq 'cassandra'
-    expect(upgrade.version).to eq 1
-    expect(upgrade.minimal_version).to eq 0
-    expect(upgrade.url).to eq 'http://cassandra.apache.org/'
-    expect(upgrade.changelog).to be_nil
-  end
+  context 'update' do
+    let(:product) { Product.new(name: 'cassandra').tap(&:save) }
+    let(:upgrade) { Upgrade.new(product_name: 'cassandra', version: 1, minimal_version: 0,
+                                url: 'http://cassandra.apache.org', changelog: 'Changelog for 1.0').tap(&:save) }
+    it 'should not be able to update product\'s name' do
+      expect(product.new?).to be false
+      expect { product.name = 'cannot update' }.to raise_error CassandraORM::CannotUpdatePrimaryKey
+    end
 
-  it 'should be able to detect uniqueness conflict' do
-    product = Product.new name: 'cassandra'
-    expect(product.save).to be true
-    product = Product.new name: 'cassandra'
-    expect(product.save).to be false
-    expect(product.errors).to match name: :unique
+    it 'should not be able to update version of the upgrade' do
+      expect(upgrade.new?).to be false
+      expect { upgrade.version = 2 }.to raise_error CassandraORM::CannotUpdatePrimaryKey
+    end
 
-    upgrade = Upgrade.new product_name: 'cassandra', version: 1,
-                          minimal_version: 0, url: 'http://cassandra.apache.org/'
-    expect(upgrade.save).to be true
-    upgrade = Upgrade.new product_name: 'cassandra', version: 1,
-                          minimal_version: 0, url: 'http://cassandra.apache.org/'
-    expect(upgrade.save).to be false
-    expect(upgrade.errors).to match [:product_name, :version] => :unique
+    it 'should be able to update url of the upgrade' do
+      expect(upgrade.new?).to be false
+      expect { upgrade.url = 'http://www.datastax.com/' }.not_to raise_error
+    end
   end
 end
