157c3e935cd8c1070bf6baed538ccf76ab8f1175
Bachue Zhou <bachue.shu@gmail.com>
2015-04-13 13:46:02 +0800

    add heartbeat

--- lib/cassandra-orm.rb
+++ lib/cassandra-orm.rb
@@ -13,5 +13,11 @@ module CassandraORM
     Model.execute identifier, cql, options
   end
 
-  module_function :configure, :connect, :execute
+  def heartbeat
+    Model.execute('heartbeat', 'SELECT NOW() FROM system.local').size == 1
+  rescue
+    false
+  end
+
+  module_function :configure, :connect, :execute, :heartbeat
 end
