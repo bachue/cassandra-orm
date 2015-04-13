66d75586c27239d839885b004e0b9d675c9fe612
Bachue Zhou <bachue.shu@gmail.com>
2015-04-13 22:27:54 +0800

    add more test cases for reconnect and heartbeat

--- spec/examples/base_spec.rb
+++ spec/examples/base_spec.rb
@@ -4,4 +4,21 @@ describe CassandraORM::Base do
     expect(CassandraORM::Base.keyspace).not_to be_nil
     expect(CassandraORM::Base.session).not_to be_nil
   end
+
+  it 'should be able to heartbeat' do
+    expect(CassandraORM.heartbeat).to be true
+  end
+
+  it 'should be able to reconnect' do
+    pids = (1..10).map do
+      fork do
+        CassandraORM.reconnect
+        Process.exit CassandraORM.heartbeat
+      end
+    end
+    pids.each do |pid|
+      _, status = Process.waitpid2 pid
+      expect(status.exitstatus).to be 0
+    end
+  end
 end
